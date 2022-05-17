30fb4aada8e3f090563f853e41c5cce822c32769
Muhamad Azroi <muhamad.azroi10@gmail.com>
2022-05-18 00:13:04 +0800

    new form ceremony

--- resources/views/user/closing/detail.blade.php
+++ resources/views/user/closing/detail.blade.php
@@ -38,25 +38,110 @@
       <b>Terms & Condition</b> <br />
       <table>
         <tr>
-          <td valign="top">1</td>
-          <td valign="top">Tiket secara resmi hanya dibeli melalui www.manoover.com . Jika Anda membeli tiket diluar itu, maka kami tidak bertanggung jawab jika tiket tidak valid. <br></td>
-          
+          <td valign="top">1.</td>
+          <td valign="top" class="rg">Tiket secara resmi hanya dibeli melalui www.manoover.com . Jika Anda membeli tiket diluar itu, maka kami tidak bertanggung jawab jika tiket tidak valid. <br></td>
+        </tr>
+        <tr>
+          <td valign="top">2.</td>
+          <td valign="top" class="rg">Panitia 3rd Manoover tidak bertanggung jawab atas kelalaian pembeli tiket yang mengakibatkan E-ticket hilang ataupun jatuh ke tangan orang lain (dalam penguasaan orang lain) untuk digunakan sebagai tanda masuk ke tempat pertunjukkan/penukaran E-ticket <br></td>
+        </tr>
+        <tr>
+          <td valign="top">3.</td>
+          <td valign="top" class="rg">Jika terjadi pembatalan pertunjukan dikarenakan alasan tertentu dikemudian hari maka uang tiket akan dikembalikan sesuai dengan ketentuan promotor. <br></td>
+        </tr>
+        <tr>
+          <td valign="top">4.</td>
+          <td valign="top" class="rg">Dalam keadaan Force Majeure seperti bencana alam, kerusuhan, perang, wabah, dan semua keadaan darurat yang diumumkan secara resmi oleh Pemerintah. Panitia/penyelenggara/promotor berhak untuk membatalkan dan/atau merubah waktu acara dan tata letak tempat tanpa pemberitahuan sebelumnya. <br></td>
+        </tr>
+        <tr>
+          <td valign="top">5.</td>
+          <td valign="top" class="rg">Panitia 3rd Manoover tidak bertanggung jawab atas kompensasi dan atau biaya pembatalan perjalanan yang anda keluarkan apabila pertunjukkan di batalkan atau di tunda. <br></td>
+        </tr>
+        <tr>
+          <td valign="top">6.</td>
+          <td valign="top" class="rg">Email yang disertakan untuk membeli tiket harus aktif, guna mengirim E-Ticket yang valid. <br></td>
+        </tr>
+        <tr>
+          <td valign="top">7.</td>
+          <td valign="top" class="rg"> E-Ticket wajib ditukar dengan wristband pada hari H di venue dengan membawa ID asli pemesan tiket. Satu wristband berlaku untuk satu orang, satu kali memasuki area pertunjukan. <br></td>
+        </tr>
+        <tr>
+          <td valign="top">8.</td>
+          <td valign="top" class="rg"> Penukaran E-ticket dapat dikonfirmasi jika pembeli menujukkan E-ticket dalam bentuk email ataupun file (tidak menerima dalam bentuk gambar ataupun lain-lain). <br></td>
+        </tr>
+        <tr>
+          <td valign="top">9.</td>
+          <td valign="top" class="rg"> Wajib membawa ID (KTP/SIM) yang valid & tidak bisa diwakilkan. <br></td>
+        </tr>
+        <tr>
+          <td valign="top">10.</td>
+          <td valign="top" class="rg"> Pembayaran dilakukan maximal 1x24 jam setelah mengisi formulir yang telah diberikan. <br></td>
+        </tr>
+        <tr>
+          <td valign="top">11.</td>
+          <td valign="top" class="rg">E-Ticket akan dikirimkan setelah melakukan pembayaran yang akan di verifikasi selambat-lambatnya 4x24 jam <br></td>
+        </tr>
+        <tr>
+          <td valign="top">12.</td>
+          <td valign="top" class="rg">Setiap pembeli hanya di perbolehkan membeli tiket maximal 5 tiket menggunakan 1 akun. <br></td>
+        </tr>
+        <tr>
+          <td valign="top">13.</td>
+          <td valign="top" class="rg">Penyelenggara berhak untuk: <br></td>
+        </tr>
+        <tr>
+          <td valign="top"></td>
+          <td valign="top" class="rg">a. Melarang Penonton masuk jika wristband telah digunakan oleh orang lain/rusak. <br></td>
+        </tr>
+        <tr>
+          <td valign="top"></td>
+          <td valign="top" class="rg">b. Memproses atau mengajukan hukuman, baik perdata maupun pidana, terhadap pengunjung yang mendapatkan Entry Pass secara tidak sah, termasuk memalsukan dan menggandakan Entry Pass yang sah atau memperoleh Entry Pass dengan cara yang tidak sesuai dengan prosedur. <br></td>
+        </tr>
+        <tr>
+          <td valign="top">14.</td>
+          <td valign="top" class="rg">Harap patuhi protokol kesehatan yang diterapkan oleh penyelenggara di area pertunjukan, cuci tangan, gunakan      masker, dan jaga jarak (3M).<br></td>
+        </tr>
+        <tr>
+          <td valign="top">15.</td>
+          <td valign="top" class="rg">Panitia 3rd Manoover mengambil tindakan tegas, dan berhak mengeluarkan pengunjung dari venue jika tidak mematuhi protokol kesehatan yang telah diterapkan.<br></td>
+        </tr>
+        <tr>
+          <td valign="top">16.</td>
+          <td valign="top" class="rg">Pembeli tiket diwajibkan telah melakukan vaksin ke-2 (booster lebih baik), serta menyiapkan peduli lindungi saat masuk ke venue.<br></td>
+        </tr>
+        <tr>
+          <td valign="top">17.</td>
+          <td valign="top" class="rg">Dilarang membawa makanan dan minuman dari luar ke dalam venue.<br></td>
         </tr>
       </table>
+
+      <br><br>
       
-      Panitia 3rd Manoover tidak bertanggung jawab atas kelalaian pembeli tiket yang mengakibatkan E-ticket hilang ataupun jatuh ke tangan orang lain (dalam penguasaan orang lain) untuk digunakan sebagai tanda masuk ke tempat pertunjukkan/penukaran E-ticket <br>
-      Jika terjadi pembatalan pertunjukan dikarenakan alasan tertentu dikemudian hari maka uang tiket akan dikembalikan sesuai dengan ketentuan promotor. <br>
-      Dalam keadaan Force Majeure seperti bencana alam, kerusuhan, perang, wabah, dan semua keadaan darurat yang diumumkan secara resmi oleh Pemerintah. Panitia/penyelenggara/promotor berhak untuk membatalkan dan/atau merubah waktu acara dan tata letak tempat tanpa pemberitahuan sebelumnya. <br>
-      Panitia 3rd Manoover tidak bertanggung jawab atas kompensasi dan atau biaya pembatalan perjalanan yang anda keluarkan apabila pertunjukkan di batalkan atau di tunda. <br>
-      Email yang disertakan untuk membeli tiket harus aktif, guna mengirim E-Ticket yang valid. <br>
-      E-Ticket wajib ditukar dengan wristband pada hari H di venue dengan membawa ID asli pemesan tiket. Satu wristband berlaku untuk satu orang, satu kali memasuki area pertunjukan. <br>
-      Wajib membawa ID (KTP/SIM) yang valid & tidak bisa diwakilkan. <br>
-      Pembayaran dilakukan maximal 1x24 jam setelah mengisi formulir yang telah diberikan. <br>
-      E-Ticket akan dikirimkan setelah melakukan pembayaran yang akan di verifikasi selambat-lambatnya 4x24 jam <br>
-      Setiap pembeli hanya di perbolehkan membeli tiket maximal 5 tiket menggunakan 1 akun. <br>
-      Penyelenggara berhak untuk: <br>
-      a. Melarang Penonton masuk jika wristband telah digunakan oleh orang lain/rusak. <br>
-      b. Memproses atau mengajukan hukuman, baik perdata maupun pidana, terhadap pengunjung yang mendapatkan Entry Pass secara tidak sah, termasuk memalsukan dan menggandakan Entry Pass yang sah atau memperoleh Entry Pass dengan cara yang tidak sesuai dengan prosedur. <br>
+      <p>
+        <b>Contact Person</b> <br />
+        Whatsapp : <br>
+        <ul>
+          <li>Risyad Fahri: 081212135321</li>
+          <li>Dienda Citta: 081511265419</li>
+        </ul>
+        Line : <br>
+        <ul>
+          <li>Rahma Indah: amaindah17</li>
+          <li>Layla Wijaya: laylaawp</li>
+        </ul>
+      </p>
+      
+      
+      
+      
+      
+     
+      
+      
+      
+      
+      
+      
      
     </div>
 
--- resources/views/user/closing/form.blade.php
+++ resources/views/user/closing/form.blade.php
@@ -7,40 +7,10 @@
   </div>
   
   <h3>Management Closing Ceremony</h3>
-  <h4>Daata Diri 1</h4>
+  <h4>Data Pembeli</h4>
 
   <form action="{{ route('closing.store') }}" method="POST" enctype="multipart/form-data" class="needs-validation" novalidate>
     @csrf
-    <div class="row">
-      <div class="col-md-6">
-        <div class="input-form">
-          <div class="tittle">Nama</div>
-          <input
-          type="text"
-          name="nama"
-          value="{{ old('nama') }}"
-          placeholder="Masukkan Nama anda..."
-        />
-            @error('nama')
-              <h6 class="text-danger mt-1 ms-2">{{ $message }}</h6>
-            @enderror
-        </div>
-      </div>
-      <div class="col-md-6">
-        <div class="input-form">
-          <div class="tittle">Email</div>
-          <input
-          type="email"
-          name="email"
-          value="{{ old('email') }}"
-          placeholder="Masukkan Email anda..."
-        />
-            @error('email')
-              <h6 class="text-danger mt-1 ms-2">{{ $message }}</h6>
-            @enderror
-        </div>
-      </div>
-    </div>
     <div class="row">
       <div class="col-md-6">
         <div class="input-form">
@@ -102,6 +72,39 @@
       </div>
     </div>
 
+    <h4>Data Diri 1</h4>
+
+    <div class="row">
+      <div class="col-md-6">
+        <div class="input-form">
+          <div class="tittle">Nama</div>
+          <input
+            type="text"
+            name="name2"
+            value="{{ old('profesi') }}"
+            placeholder="Masukkan nama peserta..."
+          />
+          @error('name2')
+            <h6 class="text-danger mt-1 ms-2">{{ $message }}</h6>
+          @enderror
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="input-form">
+          <div class="tittle">Email</div>
+          <input
+            type="email"
+            name="email2"
+            value="{{ old('email2') }}"
+            placeholder="Masukkan email peserta..."
+          />
+          @error('email2')
+            <h6 class="text-danger mt-1 ms-2">{{ $message }}</h6>
+          @enderror
+        </div>
+      </div>
+    </div>
+
     <h4>Data Diri 2</h4>
 
     <div class="row">
@@ -121,7 +124,7 @@
       </div>
       <div class="col-md-6">
         <div class="input-form">
-          <div class="tittle">Foto KTP/Kartu Pelajar</div>
+          <div class="tittle">Email</div>
           <input
             type="email"
             name="email2"
@@ -154,7 +157,7 @@
       </div>
       <div class="col-md-6">
         <div class="input-form">
-          <div class="tittle">Foto KTP/Kartu Pelajar</div>
+          <div class="tittle">Email</div>
           <input
             type="email"
             name="email3"
@@ -187,7 +190,7 @@
       </div>
       <div class="col-md-6">
         <div class="input-form">
-          <div class="tittle">Foto KTP/Kartu Pelajar</div>
+          <div class="tittle">Email</div>
           <input
             type="email"
             name="email2"
@@ -220,7 +223,7 @@
       </div>
       <div class="col-md-6">
         <div class="input-form">
-          <div class="tittle">Foto KTP/Kartu Pelajar</div>
+          <div class="tittle">Email</div>
           <input
             type="email"
             name="email2"
